7af97629e0729cd382f16100dddf6aa561904fc7
Imsung <isyou7487@naver.com>
2022-11-22 04:48:44 +0900

    Colab, Unity용 소켓 생성

--- main.py
+++ main.py
@@ -11,29 +11,22 @@ def initServerSocket() -> socket:
 
 
 def initUnitySocket() -> socket:
-    port = 19999
-    address = ('127.0.0.1', port)
-    try:
-        # UDP가 아닌 TCP 이므로 DGRAM이 아닌 STREAM을 사용
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        # TCP는 연결지향적이다.
-        s.connect(address)
-        print("Connected to address:", socket.gethostbyname(
-            socket.gethostname()) + ":" + str(port))
-        return s
-    except OSError as e:
-        print("Error while connecting :: %s" % e)
-
-        # quit the script if connection fails (e.g. Unity server side quits suddenly)
-        sys.exit()
+    HOST = '127.0.0.1'
+    PORT = 19999
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((HOST, PORT))
+    return server_socket
 
 
 if __name__ == '__main__':
-    server_socket = initServerSocket()
     unity_socket = initUnitySocket()
+    unity_socket.listen()
+    unity_socket, addr = unity_socket.accept()
+    print('unity connected')
+    server_socket = initServerSocket()
     server_socket.listen()
     colab_socket, addr = server_socket.accept()
-    print('Connected by', addr)
+    print('colab connected')
     while True:
         data = colab_socket.recv(1024)  # 소켓에서 1024 byte 만큼 가져옴
         if not data:
