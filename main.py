09476f245b765215ddf2fe15a290a8320574bb7d
Imsung <isyou7487@naver.com>
2022-11-21 22:22:57 +0900

    post mapping 추가

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+
 @app.route('/')
 def hello():
-  return 'Hello World!'
+    return 'Hello World!'
+
+
+@app.route('/pose', methods=['POST'])
+def pose():
+    params = request.get_json()
+    return ''
+
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
